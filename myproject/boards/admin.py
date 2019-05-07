630a5393b1ea883061d69e5ee12dd76c31784710
wszzy <root@wszzy.net>
2019-05-07 19:33:16 +0800

    lab-detail

--- myproject/boards/admin.py
+++ myproject/boards/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Board
+from .models import Board, Topic, Post
 
 admin.site.register(Board)
+admin.site.register(Topic)
+admin.site.register(Post)
