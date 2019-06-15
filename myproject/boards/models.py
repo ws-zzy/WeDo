d1951d2f57f283dff07f482434576438520534cb
16061182 <983414728@qq.com>
2019-06-15 21:51:52 +0800

    修改一些页面布局，修改一些url

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -24,6 +24,7 @@ class Board(models.Model):
 
 class Topic(models.Model):
     subject = models.CharField(max_length=20)
+    # created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
